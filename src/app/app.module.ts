79534994d48c4aee5ba44dc194d0729368318354
Mouhamadou Lamine Gueye <lemzogueye.lg72@gmail.com>
2022-01-28 13:26:57 +0000

    creating components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CreateQuizzComponent } from './create-quizz/create-quizz.component';
+import { ChallengeComponent } from './challenge/challenge.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateQuizzComponent,
+    ChallengeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/create-quizz/create-quizz.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-quizz',
+  templateUrl: './create-quizz.component.html',
+  styleUrls: ['./create-quizz.component.css']
+})
+export class CreateQuizzComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
