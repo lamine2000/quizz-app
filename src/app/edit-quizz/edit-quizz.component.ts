6ed7c71e7f5ba188fb02e544e80072b0de8ebece
Mouhamadou Lamine Gueye <lemzogueye.lg72@gmail.com>
2022-01-29 01:35:54 +0000

    creating components

---
+++ src/app/edit-quizz/edit-quizz.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-quizz',
+  templateUrl: './edit-quizz.component.html',
+  styleUrls: ['./edit-quizz.component.css']
+})
+export class EditQuizzComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
