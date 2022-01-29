e04f18c1584088d6d12174bf9499f915cb5ce0bf
Mouhamadou Lamine Gueye <lemzogueye.lg72@gmail.com>
2022-01-29 01:38:20 +0000

    creating routes and adding code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,42 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CreateQuizzComponent } from './create-quizz/create-quizz.component';
-import { ChallengeComponent } from './challenge/challenge.component';
+import { EditQuizzComponent } from './edit-quizz/edit-quizz.component';
+import { QuizzComponent } from './quizz/quizz.component';
+import { FourOFourComponent } from './four-o-four/four-o-four.component';
+import {RouterModule} from "@angular/router";
+import {AngularFireModule} from "@angular/fire/compat";
+import {environment} from "../environments/environment";
+import {AngularFirestoreModule} from "@angular/fire/compat/firestore";
+import {QuestionsService} from "./services/questions.service";
+
+const appRoutes = [
+  {path: '', component:CreateQuizzComponent},
+  {path: 'create-quizz', component: CreateQuizzComponent},
+  {path: 'edit-quizz/:id', component: EditQuizzComponent},
+  {path: 'quizz/:id', component: QuizzComponent},
+  {path: '**', component: FourOFourComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     CreateQuizzComponent,
-    ChallengeComponent
+    EditQuizzComponent,
+    QuizzComponent,
+    FourOFourComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes),
+
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule
   ],
-  providers: [],
+  providers: [
+    QuestionsService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
