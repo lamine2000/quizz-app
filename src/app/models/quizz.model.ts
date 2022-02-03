d714afb53c6bf1b794238fc9b5b71ddda3f5c7a0
Mouhamadou Lamine Gueye <lemzogueye.lg72@gmail.com>
2022-02-03 02:03:13 +0000

    removing todo

--- src/app/models/quizz.model.ts
+++ src/app/models/quizz.model.ts
@@ -2,7 +2,7 @@ import {Question} from "./question.model";
 
 export class Quizz{
   constructor(
-    public id: string, //todo: try to generate uuid
+    public id: string,
     public nb_players: number,
     public max_score: number,
     public questions:  Question[]
